e362118b4a15134b9be71b6d6b87dc0e139e624d
Ana Andrushchak <aandrushchak@arcanefour.com>
2026-04-02 11:33:22 -0400

    Added documentation for using forms

--- frontend/src/views/Form.vue
+++ frontend/src/views/Form.vue
@@ -6,13 +6,11 @@ import apiClient from '../services/api'
 const userToken = ref()
 
 const formsURL = ref()
-const readyToLoadForm = ref(false)
 try {
     const response = await apiClient.get('api/signUserToken')
     userToken.value = response.data.userIdToken
     const encodedUserToken = encodeURI(userToken.value)
     formsURL.value = `https://ca.makeforms.co/xx7nkai/?userToken=${encodedUserToken}`
-    readyToLoadForm.value = true
 } catch (err: any) {
     console.log('Error in example ', err)
 }
@@ -20,7 +18,7 @@ try {
 </script>
 
 <template>
-    <template v-if="readyToLoadForm">
+    <template v-if="formsURL">
         <iframe :src="formsURL" style="width: 100%;height: 500px;max-height: 100%;max-width: 100%;"
             frameborder="none"></iframe>
     </template>
